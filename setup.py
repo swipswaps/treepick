bb1578ddeeb12c24355f1c29e215dc61a2c8eb35
tslight <tobyslight@gmail.com>
2018-07-14 22:31:24 +0100

    change name calls in setup

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ppick",
+    name="ctpp",
     version="0.0.1",
     author="Toby Slight",
     author_email="tobyslight@gmail.com",
-    description="Curses based path picker",
+    description="Curses-based Tree-style Path Picker",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/tslight/ppick",
+    url="https://github.com/tslight/ctpp",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
