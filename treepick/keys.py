ea8e7f9853079df176e5b2e2dbf86840d88995fb
tslight <tobyslight@gmail.com>
2018-07-15 14:09:47 +0100

    keys should have it's own file and update readme

---
+++ treepick/keys.py
@@ -0,0 +1,55 @@
+import curses
+
+
+def parse_keys(stdscr, curline, line):
+    ESC = 27
+    action = None
+    ch = stdscr.getch()
+    if ch == ord('e') or ch == ord('q') or ch == ESC:
+        return
+    elif ch == ord('r'):
+        action = 'reset'
+    elif ch == ord('.'):
+        action = 'toggle_hidden'
+    elif ch == curses.KEY_RIGHT or ch == ord('l') or ch == ord('f'):
+        action = 'expand'
+    elif ch == curses.KEY_LEFT or ch == ord('h') or ch == ord('b'):
+        action = 'collapse'
+    elif ch == ord('L') or ch == ord('F'):
+        action = 'expand_all'
+    elif ch == ord('H') or ch == ord('B'):
+        action = 'collapse_all'
+    elif ch == ord('\t') or ch == ord('\n'):
+        action = 'toggle_expand'
+    elif ch == ord('m') or ch == ord(' '):
+        action = 'toggle_mark'
+    elif ch == ord('J') or ch == ord('N'):
+        action = 'next_parent'
+    elif ch == ord('K') or ch == ord('P'):
+        action = 'prev_parent'
+    elif ch == ord('s') or ch == ord('?'):
+        action = 'get_size'
+    elif ch == ord('S'):
+        action = 'get_size_all'
+    elif ch == curses.KEY_UP or ch == ord('k') or ch == ord('p'):
+        curline -= 1
+    elif ch == curses.KEY_DOWN or ch == ord('j') or ch == ord('n'):
+        curline += 1
+    elif ch == curses.KEY_UP or ch == ord('k') or ch == ord('p'):
+        curline -= 1
+    elif ch == curses.KEY_DOWN or ch == ord('j') or ch == ord('n'):
+        curline += 1
+    elif ch == curses.KEY_PPAGE or ch == ord('u') or ch == ord('V'):
+        curline -= curses.LINES
+        if curline < 0:
+            curline = 0
+    elif ch == curses.KEY_NPAGE or ch == ord('d') or ch == ord('v'):
+        curline += curses.LINES
+        if curline >= line:
+            curline = line - 1
+    elif ch == curses.KEY_HOME or ch == ord('g') or ch == ord('<'):
+        curline = 0
+    elif ch == curses.KEY_END or ch == ord('G') or ch == ord('>'):
+        curline = line - 1
+    curline %= line
+    return action, curline
