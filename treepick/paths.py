2cdb48d6a40854682b7c05e8d6cd813aad18ed1e
tslight <tobyslight@gmail.com>
2018-07-19 11:46:01 +0100

    if we're on root node return unchanged curline

--- treepick/paths.py
+++ treepick/paths.py
@@ -141,9 +141,12 @@ class Paths:
             return curline, self
 
     def collapse_all(self, parent, curline, depth):
-        self.prevparent(parent, curline, depth)[1].collapse()
-        self.color.curline(self.name)
-        return self.prevparent(parent, curline, depth)[0]
+        if depth > 1:
+            self.prevparent(parent, curline, depth)[1].collapse()
+            self.color.curline(self.name)
+            return self.prevparent(parent, curline, depth)[0]
+        else:
+            return curline
 
     def getpaths(self):
         '''
