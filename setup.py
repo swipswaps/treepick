211a954f2fb58c1e124e380debf0221b2b648556
tslight <tobyslight@gmail.com>
2018-07-23 08:55:15 +0100

    lazygitpush

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="treepick",
-    version="0.0.7",
+    version="0.0.8",
     author="Toby Slight",
     author_email="tobyslight@gmail.com",
     description="Curses Tree Style Path Picker",
