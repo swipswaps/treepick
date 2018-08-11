d945bb799049242668f313f54b95b58cb9fea8b8
tslight <tobyslight@gmail.com>
2018-08-11 01:21:47 +0100

    lazygitpush

---
+++ treepick/draw.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2018, Toby Slight. All rights reserved.
+# ISC License (ISCL) - see LICENSE file for details.
+
+
+class Draw(Paths):
+    def __init__(self, *args, **kwargs):
+        Paths.__init__(self, *args, **kwargs)
